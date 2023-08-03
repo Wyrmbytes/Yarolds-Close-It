97db492bf0b9a7cbd1dd996c4e0aa0a940fd85cf
Wyrmbytes <141284184+Wyrmbytes@users.noreply.github.com>
2023-08-02 20:04:20 -0400

    Update Userscript.js
    updated url list

--- Userscript.js
+++ Userscript.js
@@ -2,7 +2,7 @@
     // @name           Yarolds Close It V2
     // @namespace      Yarolds
     // @description    Closes Non Action links at Yarolds, and does Actions then Closes Action links at Yarolds
-    // @version        1.0.1
+    // @version        1.0.2
     // @copyright      2012+
     // @homepage	     http://swle.yarold.eu
     // @require        http://code.jquery.com/jquery-1.12.4.min.js
@@ -364,6 +364,7 @@
     // @include        https://virtuadopt.com/feed/*
     // @include        https://monzoo.net/myzoo.php?pseudo=*
     // @include        https://mirrorwood.com/
+    // @include        https://leetles.com/in/*
     // ==/UserScript==
      
     function randomNumber( min, max ) {
