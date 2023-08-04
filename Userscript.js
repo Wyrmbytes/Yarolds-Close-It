d64fb74450b6927ed96e7e5591e281f5c8415232
Wyrmbytes <141284184+Wyrmbytes@users.noreply.github.com>
2023-08-04 00:25:01 -0400

    Update Userscript.js

--- Userscript.js
+++ Userscript.js
@@ -2,7 +2,7 @@
     // @name           Yarolds Close It V2
     // @namespace      Yarolds
     // @description    Closes Non Action links at Yarolds, and does Actions then Closes Action links at Yarolds
-    // @version        1.0.7
+    // @version        1.0.8
     // @copyright      2012+
     // @homepage	     http://swle.yarold.eu
     // @require        http://code.jquery.com/jquery-1.12.4.min.js
