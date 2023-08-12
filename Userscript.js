a18ad4bdde35991fe994fb58e4d2a097ed008312
Wyrmbytes <141284184+Wyrmbytes@users.noreply.github.com>
2023-08-12 08:23:40 -0400

    Update Userscript.js

--- Userscript.js
+++ Userscript.js
@@ -2,7 +2,7 @@
     // @name           Yarolds Close It V2
     // @namespace      Yarolds
     // @description    Closes Non Action links at Yarolds, and does Actions then Closes Action links at Yarolds
-    // @version        1.1.0
+    // @version        1.1.1
     // @copyright      2012+
     // @homepage	     http://swle.yarold.eu
     // @require        http://code.jquery.com/jquery-1.12.4.min.js
@@ -368,6 +368,7 @@
     // @include        https://www.samuraiwar.org/refer?*
     // @include        https://eggcave.com/@*
     // @include        https://dragcave.net/image/*
+    // @include        https://formula9.ddns.net/u/*
     // @exclude https://eggcave.com/@*/shop
     // ==/UserScript==
      
